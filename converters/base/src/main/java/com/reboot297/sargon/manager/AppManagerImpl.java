0ec787a1768aaccede705320fc533732e4b0e2d8
Reboot297 <reboot297@gmail.com>
2022-09-05 14:34:53 +0300

    added base modules

---
+++ converters/base/src/main/java/com/reboot297/sargon/manager/AppManagerImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022. Viktor Pop
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.reboot297.sargon.manager;
+
+import com.reboot297.sargon.converter.AndroidConverter;
+import com.reboot297.sargon.converter.BaseConverter;
+import com.reboot297.sargon.converter.XlsConverter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implementation of the {@link AppManager}
+ */
+class AppManagerImpl implements AppManager {
+    private final Map<String, BaseConverter> converters = new HashMap<>();
+
+    AppManagerImpl() {
+        converters.put("xls", new XlsConverter());
+        converters.put("android", new AndroidConverter());
+    }
+
+
+    @Override
+    public boolean convert(String from, String to) {
+        //TODO not implemented
+        return false;
+    }
+}
